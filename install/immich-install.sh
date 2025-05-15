5d148b0d66859be9af0ca230f33ba334fa751b89
Ivan Parfentyev <pariv@mail.ru>
2025-05-15 15:33:11 +0300

    update immich install script

--- install/immich-install.sh
+++ install/immich-install.sh
@@ -165,7 +165,8 @@ msg_ok ".env файл создан"
 msg_info "Установка Immich..."
 # su - $IMMICH_USER -c "cd $REPO_DIR && ./install.sh"
 # $STD su - $IMMICH_USER -c "export REPO_DIR='$REPO_DIR' && export NVM_DIR=\"\$HOME/.nvm\" && [ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\" && cd \"\$REPO_DIR\" && ./install.sh"
-$STD su - $IMMICH_USER -c 'export REPO_DIR='$REPO_DIR' && export NVM_DIR=$HOME/.nvm && [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh && cd '$REPO_DIR' && ./install.sh'
+# $STD su - $IMMICH_USER -c 'export REPO_DIR='$REPO_DIR' && export NVM_DIR=$HOME/.nvm && [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh && cd '$REPO_DIR' && ./install.sh'
+$STD sudo -u $IMMICH_USER /bin/bash -c 'export REPO_DIR='$REPO_DIR' && export NVM_DIR=$HOME/.nvm && [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh && cd '$REPO_DIR' && ./install.sh'
 msg_ok "Immich установлен"
 
 # Настройка runtime.env
